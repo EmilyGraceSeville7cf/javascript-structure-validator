d7abe8cf0acc25b072023de918ed0b3f10150c1d
EmilyGraceSeville7cf <EmilyGraceSeville7cfg@gmail.com>
2024-03-24 05:21:38 +1000

    feat: make script more user friendly

--- GenerateDocumentation.fish
+++ GenerateDocumentation.fish
@@ -1,8 +1,14 @@
 #!/usr/bin/env fish
 
+type --query jsdoc2md || begin
+    echo (set_color red)No npm package found to generate documentation, installing...(set_color normal)
+    npm install --global jsdoc-to-markdown
+end
+
 set output Documentation.md
 
 echo >$output
 for file in *.js
+    echo (set_color green)Generating documentation for $file...(set_color normal)
     jsdoc2md $file >>$output
 end
