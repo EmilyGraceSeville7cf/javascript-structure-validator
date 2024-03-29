fe95459b7ab7aad92a6200cb89d11b2f6fd9e5f1
EmilyGraceSeville7cf <EmilyGraceSeville7cfg@gmail.com>
2024-03-30 07:36:25 +1000

    feat: ignore internal classes in docs

--- GenerateDocumentation.fish
+++ GenerateDocumentation.fish
@@ -6,9 +6,14 @@ type --query jsdoc2md || begin
 end
 
 set output Documentation.md
+set ignored_files ActionInfo_.js SimpleValidator.js ComplexValidator.js
 
 echo >$output
 for file in *.js
+    contains $file $ignored_files && begin
+        echo (set_color red)"$file is ignored"(set_color normal)
+        continue
+    end
     echo (set_color green)Generating documentation for $file...(set_color normal)
     jsdoc2md $file >>$output
 end
